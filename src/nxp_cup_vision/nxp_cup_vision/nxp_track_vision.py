5df32754419b87d10b4a4732b662d0318ebbffff
Hennzau <dev@enzo-le-van.fr>
2024-03-19 20:57:22 +0100

    simplify synapse

--- src/nxp_cup_vision/nxp_cup_vision/nxp_track_vision.py
+++ src/nxp_cup_vision/nxp_cup_vision/nxp_track_vision.py
@@ -33,40 +33,23 @@ class NXPTrackVision(Node):
         # setup CvBridge
         self.bridge = CvBridge()
 
-        # Rectangualr area to remove from image calculation to
-        # eliminate the vehicle. Used as ratio of overall image width and height
-        # "width ratio,height ratio"
-        self.maskRectRatioWidthHeight = np.array([0.0, 0.0])
-
-        # Bool for generating and publishing the debug image evaluation
-        self.debug = True
-        self.debugLineMethodUsed = False
-
-        self.timeStamp = self.get_clock().now().nanoseconds
         # Camera image size parameters
         self.imageHeight = 240
         self.imageWidth = 320
 
-        self.pts1 = np.float32([[0, self.imageHeight/3*2], [self.imageWidth/3, self.imageHeight/2], [self.imageWidth, self.imageHeight/3*2], [self.imageWidth*2/3, self.imageHeight/2]])
-        self.pts2 = np.float32([[0, self.imageHeight], [self.imageWidth/3, 0], [self.imageWidth, self.imageHeight], [self.imageWidth*2/3, 0]])
-        self.matrix = cv2.getPerspectiveTransform(self.pts1, self.pts2)
-
         # Subscribers
         self.imageSub = self.create_subscription(sensor_msgs.msg.CompressedImage,
                                                  'camera/image_raw/compressed',
-                                                 self.pixyImageCallback,
+                                                 self.camera_image_callback,
                                                  qos_profile_sensor_data)
 
         # Publishers
         self.debugDetectionImagePub = self.create_publisher(sensor_msgs.msg.CompressedImage,
                                                             "/debugImage", 0)
 
-    def pixyImageCallback(self, data):
+    def camera_image_callback(self, data):
         # Scene from subscription callback
-        scene = self.bridge.compressed_imgmsg_to_cv2(data, desired_encoding='bgr8')
-        scene = cv2.warpPerspective(scene, self.matrix, (self.imageWidth, self.imageHeight), cv2.BORDER_CONSTANT, 0)
-
-        self.debugDetectionImagePub.publish(self.bridge.cv2_to_compressed_imgmsg(scene))
+        self.debugDetectionImagePub.publish(data)
 
 
 def main(args=None):
